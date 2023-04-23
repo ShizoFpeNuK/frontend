9a2698f510b6dd0efb144b3939a8adcba2287e14
Shiza <vnchernovv02@gmail.com>
2023-04-23 14:21:10 +0300

    footer down

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,35 @@
 import './style/css/App.css';
 import { Auth } from './pages/Auth';
-import { Home } from './pages/Main';
+import { Home } from './pages/Home';
 import { Enroll } from './pages/Enroll';
 import { Header } from './components/Headers';
 import { Route, Routes } from 'react-router-dom';
+import { Col, Row, Image } from 'antd';
 
 
 function App() {
   return (
     <div className="App">
-      <header className='header'>
-        <div className="container">
-          <Header />
-        </div>
+      <header className="header">
+        <Header />
       </header>
       <main className="main">
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/enroll" element={<Enroll />} />
-            <Route path="/auth" element={<Auth />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/enroll" element={<Enroll />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
       </main>
-      <footer></footer>
+      <footer className="footer">
+        <Row className="footer_row">
+          <Col className="footer_logo" span={4}>
+            <Image src={require('./image/logo/logo1.png')} width="150px" preview={false} />
+          </Col>
+          <Col className="footer_address" span={8}>
+            Адрес головного заведения
+          </Col>
+        </Row>
+      </footer>
     </div>
   );
 }
