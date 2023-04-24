75abe8494433c4b30e2c848afb467c974216bea8
Shiza <vnchernovv02@gmail.com>
2023-04-24 22:48:24 +0300

    new services

---
+++ src/components/CardChecks.tsx
@@ -0,0 +1,20 @@
+import { Card } from "antd";
+import { CardAuth, CardBodyAuth, CardTitleAuth } from "../style/typescript/cardAuth";
+
+
+export const CardChecks = () => {
+  return (
+    <Card
+      className="auth_user_checks_cards_item"
+      title="Заказ №1"
+      style={{ textAlign: "left", ...CardAuth }}
+      headStyle={CardTitleAuth}
+      bodyStyle={CardBodyAuth}>
+      <p className="auth_user_checks_cards_item_date"> Дата и время </p>
+      <p className="auth_user_checks_cards_item_establishment"> Заведение </p>
+      <p className="auth_user_checks_cards_item_specialist"> Специалист </p>
+      <p className="auth_user_checks_cards_item_services"> Услуги </p>
+      <p className="auth_user_checks_cards_item_cost"> Стоимость услуг </p>
+    </Card>
+  )
+};
---
+++ src/components/CardService.tsx
@@ -0,0 +1,21 @@
+import { Card } from "antd";
+import { IService } from "../options/model/service.model";
+import { CardBody, CardTitle } from "../style/typescript/card";
+import { ClockCircleOutlined, DollarCircleOutlined } from "@ant-design/icons";
+
+
+interface IServiceCard {
+  service: IService;
+}
+
+
+// Доделать
+export const CardService = ({service}: IServiceCard) => {
+  return (
+    <Card title={"Услуга №" + service.service_id} headStyle={CardTitle} bodyStyle={CardBody}>
+      <p className="services_and_price_card_name"> {service.name_service} </p>
+      <p> <ClockCircleOutlined /> {service.duration} </p>
+      <p> <DollarCircleOutlined /> {service.cost} </p>
+    </Card>
+  )
+};
